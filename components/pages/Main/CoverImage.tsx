1e6a6889a00cf0d607173351b38384f3ac2da9a3
Sketch Piece <lubkin.andrey@gmail.com>
2023-01-24 00:29:01 -0800

    add m first post

--- components/pages/Main/CoverImage.tsx
+++ components/pages/Main/CoverImage.tsx
@@ -13,7 +13,7 @@ const CoverImage = ({ title, src, slug }: CoverImageProps) => {
     <Image
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-sm w-full', {
+      className={cn('shadow-sm w-full h-96 object-cover', {
         'hover:shadow-lg transition-shadow duration-200': slug
       })}
       width={1300}
